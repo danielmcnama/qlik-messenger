5b2155b2eab7df1d06a1eb1c82827a5c8633d499
Dan McNamara <daniel.mcnama@gmail.com>
2016-04-14 13:40:58 -0400

    initial commit of simple main test file

---
+++ messenger/src/main/java/org/dan/projects/messenger/test/Test.java
@@ -0,0 +1,15 @@
+package org.dan.projects.messenger.test;
+
+import org.dan.projects.messenger.model.Message;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Message message = new Message(1, "racecar", "dan", "suzy");
+		
+		System.out.println(message);
+
+	}
+
+}
